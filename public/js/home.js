1e0c7b89d0984df9a6de883b2972c062f2c744ec
vinitha <selvivini@gmail.com>
2021-03-23 20:55:53 -0500

    added edit profile logic in home.js

--- public/js/home.js
+++ public/js/home.js
@@ -3,5 +3,73 @@ $(document).ready(() => {
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
+   const UserId = data.id;
+    editProfile(UserId)
+      
   });
+
+  // const editBtn = $('updateProfile');
+  // console.log(editBtn)
+  
+  
+  // function editProfile(UserId){
+  //   console.log(UserId)
+  //   editBtn.on("click", (e)=>{
+  //     e.preventDefault();
+  //     console.log('clicked');
+  //     
+      
+  //   })
+     
+     
+     
+     
+     
+    
+  //  }
+  function editProfile(UserId){
+    $('form').on("submit",(e)=>{
+      e.preventDefault();
+       // console.log('clicked')
+       const name = $('#userName').val()
+       const highGrad = $('#userGraduation').val().trim();
+       const school = $('#userSchool').val().trim();
+       const skills =$('#userSkills').val().trim();
+       const experience = parseInt($('#userExperience').val());
+       const position = $('#userPosition').val().trim();
+       const company = $('#userCompany').val().trim();
+       const startDate = $('#userStartDate').val().trim();
+       const endDate = $('#userEndDate').val().trim();
+       const gitUserName = $('#userGitHub').val().trim();
+      
+       const ProfileObj = {
+              "name": name,
+              "highestGraduation": highGrad,
+              "school": school,
+              "skills": skills,
+              "TotalYearsOfexp": experience,
+              "currentPosition": position,
+              "companyName": company,
+              "from": startDate,
+              "to": endDate,
+              "gitUserName":gitUserName,
+              "UserId":UserId
+             }
+       
+        console.log(ProfileObj)
+       
+					$.ajax({
+						type: 'POST',
+						data: JSON.stringify(ProfileObj),
+				        contentType: 'application/json',
+                        url: '/api/profiles',						
+                        success: function(data) {
+                            console.log('success');
+                            console.log(JSON.stringify(data));
+                        }
+                    });
+     })
+  }
+  
+  
 });
